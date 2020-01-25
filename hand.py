f474e2ba5f6486bed235c14a51ee6fd69cb69258
VoxLight <tkkt392@gmail.com>
2020-01-24 19:33:44 -0500

    added card deck and hand

---
+++ hand.py
@@ -0,0 +1,19 @@
+class Hand:
+    def __init__(self, cards=[]):
+        self.cards = cards
+
+        # properties
+        self._hand_count = len(cards)
+    
+    def __iter__(self):
+        return self.cards
+
+    @property
+    def hand_count(self):
+        self._hand_count = len(self.cards)
+        return self._hand_count
+
+    def draw(self, deck):
+        card = deck.pop()
+        self.cards.append(card)
+        return card
