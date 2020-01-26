faa7e564a8f78aa091b3e02193cbd977a8ee8ece
VoxLight <tkkt392@gmail.com>
2020-01-26 02:01:08 -0500

    Added deck data to deck
    removed deck_data.json and added it to the deck.py file to conserve directory complexity and alleviate relative file issues.

--- vox_cards/deck.py
+++ vox_cards/deck.py
@@ -1,16 +1,78 @@
 # builtins
 import random
 import json
+import os
 
 # locals
-from card import Card
-from hand import Hand
+from .card import Card
+from .hand import Hand
+
+CARD_DATA = {
+    "origin": "https://gist.github.com/darthneel/00e8966f45ef2b64652985d78d11443f", 
+    "cards": [
+        {"suit": 2, "value": 2}, 
+        {"suit": 2, "value": 3}, 
+        {"suit": 2, "value": 4}, 
+        {"suit": 2, "value": 5}, 
+        {"suit": 2, "value": 6}, 
+        {"suit": 2, "value": 7}, 
+        {"suit": 2, "value": 8}, 
+        {"suit": 2, "value": 9}, 
+        {"suit": 2, "value": 10}, 
+        {"suit": 2, "value": 11}, 
+        {"suit": 2, "value": 12}, 
+        {"suit": 2, "value": 13}, 
+        {"suit": 2, "value": 1}, 
+        {"suit": 0, "value": 2}, 
+        {"suit": 0, "value": 3}, 
+        {"suit": 0, "value": 4}, 
+        {"suit": 0, "value": 5}, 
+        {"suit": 0, "value": 6}, 
+        {"suit": 0, "value": 7}, 
+        {"suit": 0, "value": 8}, 
+        {"suit": 0, "value": 9}, 
+        {"suit": 0, "value": 10}, 
+        {"suit": 0, "value": 11}, 
+        {"suit": 0, "value": 12}, 
+        {"suit": 0, "value": 13}, 
+        {"suit": 0, "value": 1}, 
+        {"suit": 1, "value": 2}, 
+        {"suit": 1, "value": 3}, 
+        {"suit": 1, "value": 4}, 
+        {"suit": 1, "value": 5}, 
+        {"suit": 1, "value": 6}, 
+        {"suit": 1, "value": 7}, 
+        {"suit": 1, "value": 8}, 
+        {"suit": 1, "value": 9}, 
+        {"suit": 1, "value": 10}, 
+        {"suit": 1, "value": 11}, 
+        {"suit": 1, "value": 12}, 
+        {"suit": 1, "value": 13}, 
+        {"suit": 1, "value": 14}, 
+        {"suit": 3, "value": 2}, 
+        {"suit": 3, "value": 3}, 
+        {"suit": 3, "value": 4}, 
+        {"suit": 3, "value": 5}, 
+        {"suit": 3, "value": 6}, 
+        {"suit": 3, "value": 7}, 
+        {"suit": 3, "value": 8}, 
+        {"suit": 3, "value": 9}, 
+        {"suit": 3, "value": 10}, 
+        {"suit": 3, "value": 11}, 
+        {"suit": 3, "value": 12}, 
+        {"suit": 3, "value": 13}, 
+        {"suit": 3, "value": 1}
+    ], 
+    "joker": [
+        {"suit": 4, "value": 0}
+    ]
+}
+
 
 
 
 class Deck:
-    with open("./deck_data.json", "r") as data:
-        deck_data = json.load(data)
+    deck_data = CARD_DATA
 
     default_deck = [Card(d) for d in deck_data["cards"]]
 
@@ -54,5 +116,5 @@ class Deck:
             self.cards.append(cut2.pop(cut2.index(random.choice(cut2))))
 
     def __iter__(self):
-        return self.cards
+        return iter(self.cards)
             
