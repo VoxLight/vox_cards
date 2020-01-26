98638a6f5b2a6e2ed99695815374e5cd0b37614f
VoxLight <tkkt392@gmail.com>
2020-01-26 04:18:07 -0500

    New Version
    Finished the basic round of testing and have the module in a working state.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vox_cards", # Replace with your own username
-    version="0.0.1",
+    version="0.1.1",
     author="VoxLight",
     author_email="tkkt392@gmail.com",
     description="A dependancy free deck, card, and hand manager.",
