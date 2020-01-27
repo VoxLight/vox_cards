7c7fd78b3a6e199fd67893ccbf42f278712f37b8
VoxLight <tkkt392@gmail.com>
2020-01-26 20:36:59 -0500

    Added generator for card_data
    Removed the dict containing the defualt card data, and in it's place is a generator that generates the 52 card values for the Card class.

--- vox_cards/deck.py
+++ vox_cards/deck.py
@@ -7,72 +7,28 @@ import os
 from .card import Card
 from .hand import Hand
 
-CARD_DATA = {
-    "cards": [
-        {"suit": 2, "value": 2}, 
-        {"suit": 2, "value": 3}, 
-        {"suit": 2, "value": 4}, 
-        {"suit": 2, "value": 5}, 
-        {"suit": 2, "value": 6}, 
-        {"suit": 2, "value": 7}, 
-        {"suit": 2, "value": 8}, 
-        {"suit": 2, "value": 9}, 
-        {"suit": 2, "value": 10}, 
-        {"suit": 2, "value": 11}, 
-        {"suit": 2, "value": 12}, 
-        {"suit": 2, "value": 13}, 
-        {"suit": 2, "value": 1}, 
-        {"suit": 0, "value": 2}, 
-        {"suit": 0, "value": 3}, 
-        {"suit": 0, "value": 4}, 
-        {"suit": 0, "value": 5}, 
-        {"suit": 0, "value": 6}, 
-        {"suit": 0, "value": 7}, 
-        {"suit": 0, "value": 8}, 
-        {"suit": 0, "value": 9}, 
-        {"suit": 0, "value": 10}, 
-        {"suit": 0, "value": 11}, 
-        {"suit": 0, "value": 12}, 
-        {"suit": 0, "value": 13}, 
-        {"suit": 0, "value": 1}, 
-        {"suit": 1, "value": 2}, 
-        {"suit": 1, "value": 3}, 
-        {"suit": 1, "value": 4}, 
-        {"suit": 1, "value": 5}, 
-        {"suit": 1, "value": 6}, 
-        {"suit": 1, "value": 7}, 
-        {"suit": 1, "value": 8}, 
-        {"suit": 1, "value": 9}, 
-        {"suit": 1, "value": 10}, 
-        {"suit": 1, "value": 11}, 
-        {"suit": 1, "value": 12}, 
-        {"suit": 1, "value": 13}, 
-        {"suit": 1, "value": 1}, 
-        {"suit": 3, "value": 2}, 
-        {"suit": 3, "value": 3}, 
-        {"suit": 3, "value": 4}, 
-        {"suit": 3, "value": 5}, 
-        {"suit": 3, "value": 6}, 
-        {"suit": 3, "value": 7}, 
-        {"suit": 3, "value": 8}, 
-        {"suit": 3, "value": 9}, 
-        {"suit": 3, "value": 10}, 
-        {"suit": 3, "value": 11}, 
-        {"suit": 3, "value": 12}, 
-        {"suit": 3, "value": 13}, 
-        {"suit": 3, "value": 1}
-    ], 
-    "joker": {"suit": 4, "value": 0}
-}
+def generate_card_data():
+    suits = 3
+    values = 13
+    cur_suit = 0
+    cur_value = 1
+    while cur_suit <= suits and cur_value <= values:
+        yield {"suit":cur_suit, "value":cur_value}
+        if cur_value == values:
+            cur_suit += 1
+            cur_value = 0
+        cur_value += 1
+        
+
+JOKER = {"suit": 4, "value": 0}
 
 
 
 
 class Deck:
-    deck_data = CARD_DATA
 
-    default_deck = [Card(d) for d in deck_data["cards"]]
-    joker = Card(deck_data["joker"])
+    default_deck = [Card(d) for d in generate_card_data()]
+    joker = Card(JOKER)
 
     def __init__(self, hands=1, jokers=False):
         self.cards = Deck.default_deck
