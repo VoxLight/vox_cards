b1e5a4abb72b2f15dafb74a7a22d9756c2958943
VoxLight <tkkt392@gmail.com>
2020-01-26 21:38:13 -0500

    Updated version
    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vox_cards", # Replace with your own username
-    version="0.1.5",
+    version="0.1.6",
     author="VoxLight",
     author_email="tkkt392@gmail.com",
     description="A dependancy free deck, card, and hand manager.",
